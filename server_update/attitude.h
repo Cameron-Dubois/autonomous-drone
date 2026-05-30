a3c8b4b9f46e7a73efa4f5b5ec2256e7475147ba
Abhiram Sai Yegalapati <ayegalap@ucsc.edu>
2026-05-29 23:06:25 -0700

    Clean up attitude.h by removing comments
    Removed comments and cleaned up the header file.

--- server_update/attitude.h
+++ server_update/attitude.h
@@ -1,6 +1,3 @@
-/*
- * attitude.h - complementary-filter attitude estimator
- */
 #ifndef ATTITUDE_H_
 #define ATTITUDE_H_
 
@@ -12,10 +9,8 @@ typedef struct {
     float yaw;      /* deg, +CCW from above (free-running, no magnetometer) */
 } attitude_t;
 
-/* Reset the integrator. Call once after init. */
 void attitude_reset(void);
 
-/* Advance the filter by one sample.  dt should be LOOP_DT_S. */
 void attitude_update(const imu_sample_t *s, float dt, attitude_t *out);
 
 #endif /* ATTITUDE_H_ */
