9f7500f08b995c8389de2dda83c2ce18dd5e535f
Abhiram Sai Yegalapati <ayegalap@ucsc.edu>
2026-05-29 23:07:56 -0700

    Remove comment from flight_task.h
    Removed comment about task initialization order.

--- server_update/flight_task.h
+++ server_update/flight_task.h
@@ -2,8 +2,6 @@
 #define FLIGHT_TASK_H_
 #include "esp_err.h"
 
-/* Spawn the high-priority flight task.  Must be called after
- * imu_init / motors_init / wifi_link_init. */
 esp_err_t flight_task_start(void);
 
 #endif /* FLIGHT_TASK_H_ */
